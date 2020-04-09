9927f5b8f97db5af1ed3c9de8a32f2666edfe039
ghassen charni <ghassen.cherni@gmail.com>
2020-04-09 12:07:38 +0200

    Add configMap in the deployment wordpress

--- deploy-wordpress.sh
+++ deploy-wordpress.sh
@@ -1,14 +1,25 @@
 #/bin/bash
 
 # Loads the needed parms to connect to our EKS (kubeconfig file) and to allow wordpress to reach our DB (RDS AWS) 
-source ../terraform_aws_eks/rds_kubeconfig.env
+export KUBECONFIG=config
 
+# Create the configmap that contains the RDS connexion params
+echo "Creating ConfigMap .."
+kubectl create -f rds_conn_configmap.yaml
 
 # Create a persisant volume to be shared between PODS
-kubectl create -f persistentVolume.yml
+echo "Creating Persistent Volume .."
+kubectl create -f persistentVolume.yaml
+sleep 5
 
 # Create the deployement ( by default it runs two PODS )
+echo "Creating the wordpress deployment .." 
 kubectl create -f deployment-wordpress.yaml
+sleep 20
+echo "Wordpress deployment created"
 
 # Create the loadbalance service, AWS will automatically build an ELB 
-kubectl create -f service_wordpress.yml
+echo "Creating the wordpress ELB .."
+kubectl create -f service_wordpress.yaml
+sleep 10
+echo "Wordpress ELB created"
